8c970a70c17fb3c0bf498a67c0e8a6258127c67c
Alinasarwary <alinasoroush96@gmail.com>
2022-01-14 13:30:01 +0430

    citizen

---
+++ Front-End/src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="parag">
+        <h3>About Project</h3>
+        <p >Blockchain is technology that builds a trustworthy service in an untrustworthy environment, It uses replication of distributed systems to build a decentralized service that achieves the same goals with a trusted centralized one.<br>
+        My goal and idea oa making this project is to register the citizens of a country, the pillar of any country is its citizens, and when foreigners enter a country without an identity, it causes many unfortunate incidents.<br/>
+        I live in Afghanistan, a country that has many foreigners and profiteers without a clear identity, as a result of which we always see a lot of bloody wars. Having a web-app that can register people from different regions is one of the requirements of my country. When people want to travel abroad from other countries or people inside the country, they go through an identification stage and through Registering in a secure network such as blockchain that no longer allows humans to forge work. because people of a government have the right to peaceful life, all over peace, and without mental tensions.</p><br/>
+        <h6><b><i>please follow me on:</i></b></h6>
+        &emsp; GitHub - <a href="https://github.com/Alinasarwary">Alinasarwary</a><br/>
+        &emsp; Twitter - <a href="https://twitter.com/alina_sarwary">alina_sarwary</a><br/>
+        &emsp; LinkedIn - <a href="https://linkedin.com/in/alina-sarwari-79a10621b">Alina Sarwary</a>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.parag{
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    width: 80%;
+    margin: auto;
+    direction: ltr;
+    background-color: white;
+    color:black;
+    padding: 3%;
+    font-size:18px;
+}
+a{
+    color: goldenrod;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+</style>
